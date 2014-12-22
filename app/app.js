7bfa60b375cde6905b3629dfb63d45a21de7c3f0
dannielle <dannielle.del@gmail.com>
2014-12-22 15:38:47 -0500

    Removing old files

--- app/app.js
+++ app/app.js
@@ -3,9 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('sdsApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version',
   'sds.search',
   'sds.falling',
   'filters-module'
@@ -21,5 +18,5 @@ angular.module('sdsApp', [
         controller: 'fallingController'
       }).
       otherwise({redirectTo: "/search.html"});
-}])
+}]);
 
