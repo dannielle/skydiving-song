d9570a0e8b9ffb76e1fff865f1bf70a8d2f90a03
dannielle <dannielle.del@gmail.com>
2015-01-03 12:56:53 -0500

    Video playlist plays from the list of videos

--- app/components/falling/falling.js
+++ app/components/falling/falling.js
@@ -4,19 +4,19 @@ angular.module('sds.falling', [])
         var url = $scope.videoId;
         var string = '<div id="bgndVideo" class="player" data-property="{videoURL:\'' + url + '\',containment:\'body\',autoPlay:true, mute:false, startAt:0, opacity:0.5}"></div>';
         $("#player-container").append(string);
-        $(".player").YTPlayer();
+        //$(".player").YTPlayer();
 
         //GET the list of video IDs, the names, and other information
         //make the info array
         // [{videoId: foo, name: bar, passion: baz}, {}]
         //make the video array
         var videos = [
-            {videoURL:"VuaJAgx0x_4",containment:'body',autoPlay:true, mute:false, startAt:0,opacity:1, loop:false, ratio:"4/3", addRaster:true},
+            {videoURL: url,containment:'body',autoPlay:true, mute:false, startAt:0,opacity:1, loop:false, ratio:"4/3", addRaster:true},
             {videoURL: "3ovA7zeviRo",containment:'body',autoPlay:true, mute:true, startAt:0,opacity:1, loop:false, ratio:"4/3", addRaster:false},
             {videoURL: "u9k1FaMIYTs",containment:'body',autoPlay:true, mute:false, startAt:0,opacity:1, loop:false, ratio:"4/3", addRaster:true}
         ];
         //play the video array
-        //jQuery("#player-container").YTPlaylist(videos, true);
+        jQuery("#player-container").YTPlaylist(videos, false);
 
         //POST to save video ID, name, and info
 
