e5cdf0bd0ac46bc98aaae3b0e7e64e23424fd037
dannielle <dannielle.del@gmail.com>
2015-01-03 14:19:17 -0500

    better pseudo code for remaining work

--- app/components/falling/falling.js
+++ app/components/falling/falling.js
@@ -2,23 +2,24 @@ angular.module('sds.falling', [])
     .controller('fallingController', ["$scope", "$routeParams", function($scope, $routeParams){
         $scope.videoId = $routeParams.videoId;
         var url = $scope.videoId;
-        var string = '<div id="bgndVideo" class="player" data-property="{videoURL:\'' + url + '\',containment:\'body\',autoPlay:true, mute:false, startAt:0, opacity:0.5}"></div>';
+        //var string = '<div id="bgndVideo" class="player" data-property="{videoURL:\'' + url + '\',containment:\'body\',autoPlay:true, mute:false, startAt:0, opacity:0.5}"></div>';
         $("#player-container").append(string);
         //$(".player").YTPlayer();
 
+        var videos = [{videoURL: url,containment:'body',autoPlay:true, mute:false, startAt:0,opacity:1, loop:false, ratio:"4/3", addRaster:true}];
+
         //GET the list of video IDs, the names, and other information
-        //make the info array
-        // [{videoId: foo, name: bar, passion: baz}, {}]
-        //make the video array
-        var videos = [
-            {videoURL: url,containment:'body',autoPlay:true, mute:false, startAt:0,opacity:1, loop:false, ratio:"4/3", addRaster:true},
-            {videoURL: "3ovA7zeviRo",containment:'body',autoPlay:true, mute:true, startAt:0,opacity:1, loop:false, ratio:"4/3", addRaster:false},
-            {videoURL: "u9k1FaMIYTs",containment:'body',autoPlay:true, mute:false, startAt:0,opacity:1, loop:false, ratio:"4/3", addRaster:true}
-        ];
+        //iterate through the result list
+            //make the info array
+            // [{videoId: foo, name: bar, passion: baz}, {}]
+            //add to the video array
+        //end iterate
+
+        //POST to save video ID, name, and info
+        
         //play the video array
         jQuery("#player-container").YTPlaylist(videos, false);
 
-        //POST to save video ID, name, and info
 
 
         setTimeout(function() {
